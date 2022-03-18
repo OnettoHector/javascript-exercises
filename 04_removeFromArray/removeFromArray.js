1546028e9f69615657aa3e3b01d35c0d9c4a196b
Hector <bectorgames@hotmail.com>
2022-03-18 16:31:01 -0300

    Up to 4 done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,...moreArgs) {
+
+    let newArray = [];
+    
+    for (let i=0;i<array.length;i++){
+        
+        for (let j=0;j<moreArgs.length;j++){
+        if(array[i] === moreArgs[j]){
+        newArray[i] = "undefined";
+        break;
+    }
+        else newArray[i] = array[i];
+    }
+    }
+
+    newArray = newArray.filter(item => item != "undefined");
+    return newArray;
 
 };
 

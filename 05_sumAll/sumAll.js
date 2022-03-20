6913f0511fc9ab90d9178436c213b3c91b448c58
Hector <bectorgames@hotmail.com>
2022-03-20 19:19:30 -0300

    First batch done!

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(start,finish) {
 
+let largrNmbr;
+let smllrNmbr;
+let result = 0;
+
+if(start < 0 || finish < 0 || typeof(start) != "number" || typeof(finish) != "number"){
+    return "ERROR";
+}
+else if (start > finish){
+    largrNmbr = start;
+    smllrNmbr = finish;
+}
+else{
+    smllrNmbr = start;
+    largrNmbr = finish;
+}
+
+for (let i = smllrNmbr;i<=largrNmbr;i++)
+{
+    result +=i;
+}
+return result;
 };
 
 // Do not edit below this line
